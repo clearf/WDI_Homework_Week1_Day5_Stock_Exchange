815c428db45cd0d371bd84e685725686953f5040
Beryl <beryl.schragger@gmail.com>
2013-06-22 10:27:05 -0400

    Remove old spec file and change name of second Stock case

--- spec/finance_spec.rb
+++ spec/finance_spec.rb
@@ -10,12 +10,14 @@ describe Stock, "#get_share_price" do
     apple_test_price.should == (apple.share_price)
   end
 
-  it "should return Apple's share price" do
+  it "should return Microsoft's share price" do
     microsoft = Stock.new('MSFT')
     microsoft_test_price = YahooFinance::get_quotes(YahooFinance::StandardQuote, 'MSFT')['MSFT'].lastTrade
     microsoft_test_price.should == (microsoft.share_price)
   end
 end
+
+describe
 
 
 
