8611c16bc3e65ff6d05c66340b6588d61a93db76
Christopher Wright <cmwright33@gmail.com>
2013-06-23 15:02:31 -0400

    v1 portfolio

--- lib/portfolio.rb
+++ lib/portfolio.rb
@@ -1,2 +1,29 @@
+
+require_relative 'stock.rb'
+require_realtive 'yahoofinance'
+
+
 class Portfolio
+ attr_accessor :portfolio_name, :portfolio, :stocks, :value
+
+  def initialize(portfolio_name, portfolio = {})
+    @portfolio_name = portflio_name
+    @value = value
+  end
+
+  def delete_stock
+    portfolio.delete(stock)
+  end
+
+  def add_stock
+    portfolio << stock
+  end
+
+
+  def calculate_value
+    value = 0
+    portfolio.each do |stock|
+    value += stock.total_price
+    end
+  end
 end
