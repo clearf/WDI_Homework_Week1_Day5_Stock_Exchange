41967376c092d7991996e318207a60e774fce6f2
Beryl <beryl.schragger@gmail.com>
2013-06-24 03:47:44 -0400

    Completed assignment

--- spec/finance_spec.rb
+++ spec/finance_spec.rb
@@ -1,8 +1,6 @@
 require 'finance'
 require 'YahooFinance'
 
-
-
 describe Stock, "#get_share_price" do
   it "should return Apple's share price" do
     apple = Stock.new('AAPL')
@@ -15,11 +13,32 @@ describe Stock, "#get_share_price" do
     microsoft_test_price = YahooFinance::get_quotes(YahooFinance::StandardQuote, 'MSFT')['MSFT'].lastTrade
     microsoft_test_price.should == (microsoft.share_price)
   end
+
+  it "should return a price of zero for a non-existent stock" do
+    blank = Stock.new('WRONG')
+    blank_test_price = YahooFinance::get_quotes(YahooFinance::StandardQuote, 'WRONG')['WRONG'].lastTrade
+    blank_test_price.should == (0)
+  end
+
+  # it "should return an error message for a null-value stock name" do
+  #   blank = Stock.new('')
+  #   blank_test_price = YahooFinance::get_quotes(YahooFinance::StandardQuote, '')[''].lastTrade
+  #   rescue NoMethodError
+  #     true
+  #   end
+  # end
 end
 
-describe
+describe Stock, "#buy_stock" do
 
+end
 
+# describe Portfolio, "#get_portfolio" do
+  # it "should return the portfolio" do
+  # end
+# end
 
+# describe Client, "#get_account_info" do
+# end
 
 
